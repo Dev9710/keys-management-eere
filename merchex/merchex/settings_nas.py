21104c4f258667301659c7c1ed376b17233dd0fc
Dev9710 <ludo_du_97.2@hotmail.com>
2026-01-15 16:10:34 +0100

    edit settings nas for debug

--- merchex/merchex/settings_nas.py
+++ merchex/merchex/settings_nas.py
@@ -43,8 +43,8 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
 
 # Si tu testes en HTTP direct (http://IP:8002), mettre False temporairement
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 
 # -----------------------------
 # LOGGING (Docker stdout)
