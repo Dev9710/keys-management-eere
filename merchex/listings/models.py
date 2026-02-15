cc9b2aa5acb8bdcbd420f01c4f0a7ed82b7aa6e4
Dev9710 <ludo_du_97.2@hotmail.com>
2026-02-15 04:29:34 +0100

    edit bdd + migrate + migrations

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -112,7 +112,7 @@ class ActionLog(models.Model):
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.name)
