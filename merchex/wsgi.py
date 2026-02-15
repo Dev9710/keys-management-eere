890380a8362e70fbcca661f273f7a5841419250d
Dev9710 <ludo_du_97.2@hotmail.com>
2026-02-15 22:19:55 +0100

    add ver env to wsgi

--- merchex/wsgi.py
+++ merchex/wsgi.py
@@ -8,6 +8,10 @@ if PROJECT_DIR not in sys.path:
     sys.path.insert(0, PROJECT_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "merchex.settings_nas")
-
+os.environ.setdefault("MYSQL_HOST", "merchex-db")
+os.environ.setdefault("MYSQL_PORT", "3306")
+os.environ.setdefault("MYSQL_DATABASE", "merchex")
+os.environ.setdefault("MYSQL_USER", "merchex")
+os.environ.setdefault("MYSQL_PASSWORD", "password")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
